14d78742bf1670f70fdc5c6e32044e8cea11016a
seo003 <sy030212@naver.com>
2024-10-30 15:47:15 +0900

    naver

--- crawling/1_naver.py
+++ crawling/1_naver.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import re
 
-search = "마루"
+search = "마그네틱"
 
 # 웹사이트 URL 설정
 url = "https://search.naver.com/search.naver?ssc=tab.news.all&where=news&sm=tab_jum&query=" + search + "&sort=date"
@@ -16,19 +18,43 @@ if response.status_code == 200:
     # 뉴스 데이터 div 선택
     news_list = soup.select('#main_pack > section > div.api_subject_bx > div.group_news > ul > li')
 
+    # 현재 시간
+    now = datetime.now()
+
     for news in news_list:
         # 제목과 링크
         title_tag = news.select_one('div.news_wrap.api_ani_send > div > div.news_contents > a.news_tit')
         title = title_tag['title']
         link = title_tag['href']
 
-        # 날짜
-        date = news.select_one('div.news_wrap.api_ani_send > div > div.news_info > div.info_group > span').text.strip()
+        # 날짜 전처리
+        date_text = news.select_one(
+            'div.news_wrap.api_ani_send > div > div.news_info > div.info_group > span').text.strip()
+
+        if "시간 전" in date_text:
+            hours_ago = int(re.search(r'\d+', date_text).group())
+            news_time = now - timedelta(hours=hours_ago)
+            formatted_date = news_time.strftime("%Y.%m.%d")
+        elif "분 전" in date_text:
+            minutes_ago = int(re.search(r'\d+', date_text).group())
+            news_time = now - timedelta(minutes=minutes_ago)
+            formatted_date = news_time.strftime("%Y.%m.%d")
+        elif "일 전" in date_text:
+            days_ago = int(re.search(r'\d+', date_text).group())
+            news_time = now - timedelta(days=days_ago)
+            formatted_date = news_time.strftime("%Y.%m.%d")
+        elif "주 전" in date_text:
+            weeks_ago = int(re.search(r'\d+', date_text).group())  # 숫자 부분 추출
+            news_time = now - timedelta(weeks=weeks_ago)
+            formatted_date = news_time.strftime("%Y.%m.%d")
+        else:
+            # "YYYY.MM.DD" 형식
+            formatted_date = date_text
 
         # 출력
         print(f"제목: {title}")
         print(f"링크: {link}")
-        print(f"시간: {date}")
+        print(f"날짜: {formatted_date}")
         print("-" * 100)
 else:
-    print("naver 크롤링에 실패하였습니다.")
+    print(f"Failed to fetch the page, status code: {response.status_code}")
