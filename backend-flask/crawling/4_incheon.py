c6c42b86e7f501a2fe280d171647184f90a2b3a7
seo003 <sy030212@naver.com>
2024-11-04 16:45:32 +0900

    incheon

--- backend-flask/crawling/4_incheon.py
+++ backend-flask/crawling/4_incheon.py
@@ -14,34 +14,33 @@ def getdata():
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # 뉴스 데이터 선택
-        news_list = soup.select('#content2024 .board-blog-list ul li')
+        news_list = soup.select('.board-article-group')  # 'board-article-group' 클래스의 요소 선택
 
         result = []
         for news in news_list:
-            # 링크와 제목
-            link_tag = news.select_one('a')  # 각 뉴스 항목의 링크
+            # 링크 추출
+            link_tag = news.find_parent('a')
             if link_tag:
-                link_str = link_tag['href']
-                link = "https://www.incheon.go.kr" + link_str
-                title = link_tag.select_one('.subject').text.strip()
+                link = "https://www.incheon.go.kr" + link_tag['href']
+
+            # 제목 추출
+            title_tag = news.select_one('.subject')
+            if title_tag:
+                title = title_tag.text.strip()
 
             # 날짜 정보 추출
-            date_tag = news.select_one('.board-item-area dt')
-            date = date_tag.find_next_sibling('dd').text.strip()
-
-            # 날짜 형식 변환 (YYYY-MM-DD → YYYY.MM.DD)
-            date = date.replace('-', '.')
+            date_tag = news.select_one('.board-item-area dt:-soup-contains("제공일자")')
+            if date_tag:
+                date = date_tag.find_next_sibling('dd').text.strip()
+                date = date.replace('-', '.')
 
             # 출력
-            #print(f"제목: {title}")
-            #print(f"링크: {link}")
-            #print(f"날짜: {date}")
-            #print("-" * 100)
+            print(f"제목: {title}")
+            print(f"링크: {link}")
+            print(f"날짜: {date}")
+            print("-" * 100)
             dict_data = {"title": title, "link": link, "date": date}
             result.append(dict_data)
-
         return result
     else:
         print(f"Failed to fetch the page, status code: {response.status_code}")
-
-
